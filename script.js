a81bf287322aa625d81db5c8a3bc327f003735fc
0xKonstantin <119298586+0xKonstantin@users.noreply.github.com>
2024-07-11 00:06:56 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+//variables
+let input = prompt("Please provide the month you were born");
+//code
+if(input.toLowerCase() === "january"){
+    console.log('♑	Capricorn	January')
+}else if(input.toLowerCase() === "february"){
+    console.log('♒	Aquarius	February')
+}else if(input.toLowerCase() === "march"){
+    console.log('♓	Pisces	March')
+}else if(input.toLowerCase() === "april"){
+    console.log('♈	Aries	April')
+}else if(input.toLowerCase() === "may"){
+    console.log('♉	Taurus	May')
+}else if(toLoinput.toLowerCase() === "june"){
+    console.log('♊	Gemini	June')
+}else if(input.toLowerCase() === "july"){
+    console.log('♋	Cancer	July')
+}else if(input.toLowerCase() === "august"){
+    console.log('♌	Leo	August')
+}else if(input.toLowerCase() === "september"){
+    console.log('♍	Virgo	September')
+}else if(input.toLowerCase() === "october"){
+    console.log('♎	Libra	October')
+}else if(input.toLowerCase() === "november"){
+    console.log('♏	Scorpio	November')
+}else if(input.toLowerCase() === "december"){
+    console.log('♐	Sagittarius	December')
+}
+
+Export;
